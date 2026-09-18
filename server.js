1d3b586ab5d34fb1d053f900f2455c9daa2c36f7
Romi <halleluyahajiboye@gmail.com>
2026-09-18 04:38:53 -0700

    Allow credentialed CORS preflight

--- server.js
+++ server.js
@@ -213,6 +213,8 @@ function json(res, statusCode, data, extraHeaders = {}) {
       "https://rizora.com.ng",
     "Access-Control-Allow-Headers":
       "Content-Type, Authorization",
+        "Access-Control-Allow-Credentials": "true",
+
     "Access-Control-Allow-Methods":
       "GET, POST, PUT, PATCH, DELETE, OPTIONS",
     "Access-Control-Allow-Credentials": "true",
