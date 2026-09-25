202c7f4055b400564d5a491989d43a214ea52584
Romi-noir <princeromi2008@gmail.com>
2026-09-25 06:03:03 -0700

    Wire creator workflow backend into v2 router

--- rizora-v2-backend.js
+++ rizora-v2-backend.js
@@ -1,3 +1,4 @@
+const { handleRizoraGrowth } = require("./rizora-v2-growth");
 const { handleRizoraPlatform } = require("./rizora-v2-platform");
 "use strict";
 
@@ -176,6 +177,7 @@ async function handleRizoraV2(ctx){
     if(rb.resetWarnings===true){var rs=warningState(db,rt.id);rs.count=0;rs.history=[];}
     ctx.audit(db,"account_restored",user,{targetUserId:rt.id,resetWarnings:rb.resetWarnings===true});ctx.saveDB(db);ctx.sendJSON(res,200,{success:true});return true;
   }
+  if (await handleRizoraGrowth(ctx)) return true;
   if (await handleRizoraPlatform(ctx)) return true;
   return false;
 }
