69bf0d737c02787023bfdc8339a043ed87834bb0
Romi-noir <princeromi2008@gmail.com>
2026-09-26 06:59:22 -0700

    Restore AI rate limiter state on frontend branch

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ const SUPER_ADMINS = new Set([
 
 const loginAttempts = new Map();
 const signupAttempts = new Map();
+const aiRateLimits = new Map();
 
 const LOGIN_WINDOW_MS = 15 * 60 * 1000;
 const LOGIN_MAX_ATTEMPTS = 10;
